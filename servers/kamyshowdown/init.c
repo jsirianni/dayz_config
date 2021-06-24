3bd336870b6692c2025ff10f57bd5e563db37952
jsirianni <joe.sirianni@observiq.com>
2021-06-23 22:48:08 -0400

    random vest

--- servers/kamyshowdown/init.c
+++ servers/kamyshowdown/init.c
@@ -86,6 +86,7 @@ class CustomMission: MissionServer
 	{
 		TStringArray top = {"PrisonUniformJacket","BDUJacket","NurseDress_Blue","Raincoat_Green","Raincoat_Pink"};
 		TStringArray pants = {"PrisonUniformPants","BDUPants","CargoPants_Beige","ShortJeans_Blue"};
+		TStringArray vest = {"PlateCarrierVest"}
 		TStringArray shoes = {"AthleticShoes_Black","AthleticShoes_Brown","AthleticShoes_Grey","HikingBootsLow_Beige","HikingBootsLow_Black","HikingBootsLow_Grey","HikingBoots_Black","HikingJacket_Black"};
 		TStringArray gun = {"Flaregun","MakarovIJ70","FNX45","Glock19","MKII","Colt1911","Engraved1911","Izh18","Mosin9130","CZ527","Winchester70","SKS", "Mp133Shotgun","Izh43Shotgun","Saiga", "CZ61","UMP45","MP5K","AKS74U", "FAL","AKM","AK101","AK74","M4A1","VSS","B95","SVD"};
 		TStringArray mellee = {"BrassKnuckles_Shiny","StunBaton","CattleProd",  "BaseballBat", "NailedBaseballBat"};
@@ -95,6 +96,10 @@ class CustomMission: MissionServer
 		player.GetInventory().CreateInInventory(top.GetRandomElement());
 		player.GetInventory().CreateInInventory(pants.GetRandomElement());
 		player.GetInventory().CreateInInventory(shoes.GetRandomElement());
+
+		if (decide(50) == true) {
+			player.GetInventory().CreateInInventory(vest.GetRandomElement());
+		}
 
 		switch(gun.GetRandomElement()) {
 		  case "Flaregun":
