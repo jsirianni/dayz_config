5f380f63c84c74c01eb13b789a55d7b9bb2963cb
jsirianni <joe.sirianni@observiq.com>
2021-06-24 20:10:57 -0400

    sync server name

--- servers/kamyshowdown/start.bat
+++ servers/kamyshowdown/start.bat
@@ -1,7 +1,7 @@
 @echo off
 :start
 
-set serverName=Kamyshowdown
+set serverName=Kamyshowdown | Vanilla | DogShitGames.com
 set serverLocation="C:\dayz"
 set serverPort=2302
 set serverConfig=serverDZ.cfg
