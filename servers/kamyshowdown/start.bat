952cc86550daff6900ca0710b4482b7564eab67b
Joseph Sirianni <joe.sirianni@bluemedora.com>
2021-06-25 20:20:43 -0400

    Config (#13)
    * default config with server name
    
    * turn off 3rd person and crosshairs
    
    * max time speed
    
    * persist time between restarts
    
    * configure queue settings
    
    * Add startup script
    
    * sync server name
    
    * quote because pipes

---
+++ servers/kamyshowdown/start.bat
@@ -0,0 +1,19 @@
+@echo off
+:start
+
+set serverName="Kamyshowdown | Vanilla | DogShitGames.com"
+set serverLocation="C:\dayz"
+set serverPort=2302
+set serverConfig=serverDZ.cfg
+set serverCPU=4
+
+title %serverName% batch
+cd "%serverLocation%"
+start "DayZ Server" /min "DayZServer_x64.exe" -config=%serverConfig% -port=%serverPort% -cpuCount=%serverCPU% -dologs -adminlog -netlog -freezecheck
+echo (%time%) %serverName% started.
+
+timeout 14390
+taskkill /im DayZServer_x64.exe /F
+timeout 10
+
+goto start
