d0ac40b45b6350054e5cfe1a0ad942c6a2bc0a5d
jsirianni <joe.sirianni@observiq.com>
2021-06-26 12:19:52 -0400

    test important values in globals xml

--- servers/kamyshowdown/test/globals_xml_test.go
+++ servers/kamyshowdown/test/globals_xml_test.go
@@ -2,6 +2,7 @@ package kamyshowdown
 
 import (
 	"encoding/xml"
+	"fmt"
 	"io/ioutil"
 	"os"
 	"testing"
@@ -33,4 +34,26 @@ func TestGlobalsXML(t *testing.T) {
 	g := Globals{}
 	err = xml.Unmarshal(b, &g)
 	require.NoError(t, err)
+
+	for _, line := range g.Var {
+		if len(line.Name) == 0 {
+			require.NotEmpty(t, line.Name, "expected name to not be empty")
+			return
+		}
+
+		switch name := line.Name; name {
+		case "AnimalMaxCount":
+			require.Equal(t, "0", line.Value, fmt.Sprintf("expected %s to be 0", name))
+		case "CleanupLifetimeDeadPlayer":
+			require.Equal(t, "20", line.Value, fmt.Sprintf("expected %s to be 0", name))
+		case "CleanupLifetimeRuined":
+			require.Equal(t, "60", line.Value, fmt.Sprintf("expected %s to be 0", name))
+		case "TimeLogin":
+			require.Equal(t, "3", line.Value, fmt.Sprintf("expected %s to be 0", name))
+		case "TimeLogout":
+			require.Equal(t, "3", line.Value, fmt.Sprintf("expected %s to be 0", name))
+		case "ZombieMaxCount":
+			require.Equal(t, "0", line.Value, fmt.Sprintf("expected %s to be 0", name))
+		}
+	}
 }
