f45a37f918e72af4059404ba5e099986d61d0d29
Joseph Sirianni <joe.sirianni@bluemedora.com>
2024-04-04 20:55:53 -0400

    more spawns (#30)
    * more spawns
    
    * fix tests
    
    * Fix test
    
    * Fix test
    
    * fix test

--- servers/kamyshowdown/test/cfgplayerspawnpoints_xml_test.go
+++ servers/kamyshowdown/test/cfgplayerspawnpoints_xml_test.go
@@ -132,13 +132,13 @@ func TestCFGPlayerSpawnPointsXML(t *testing.T) {
 		z, err := strconv.ParseFloat(spawn.Z, 32)
 		require.NoError(t, err, "expected Y spawn coordinate to parse as a float64")
 
-		var maxX float64 = 12249
+		var maxX float64 = 12250
 		var minX float64 = 11840
 		require.Less(t, x, maxX, fmt.Sprintf("expected X axis value to be less than %v", maxX))
 		require.Greater(t, x, minX, fmt.Sprintf("expected X axis value to be greater than %v", minX))
 
-		var maxZ float64 = 3530
-		var minZ float64 = 3440
+		var maxZ float64 = 4000
+		var minZ float64 = 3000
 		require.Less(t, z, maxZ, fmt.Sprintf("expected Z axis value to be less than %v", maxZ))
 		require.Greater(t, z, minZ, fmt.Sprintf("expected Z axis value to be greater than %v", minZ))
 	}
