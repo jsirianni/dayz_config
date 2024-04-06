a5cc1d4b43c36aaab6ff826e51195c668cd5cf7d
Joseph Sirianni <joe.sirianni@bluemedora.com>
2024-04-06 17:44:27 -0400

    Integration tests (#32)
    * try ci
    
    * try creds
    
    * comment
    
    * rename
    
    * try service and new install dir, with user
    
    * deploy config mods
    
    * move init.c
    
    * fix sed
    
    * fix sed
    
    * join
    
    * limit
    
    * rename
    
    * skip render
    
    * dayz user home
    
    * readme
    
    * check log output
    
    * extend time
    
    * Debug structure
    
    * remove tree
    
    * check log without custom configs
    
    * try build essential
    
    * fix systemd
    
    * fix grep
    
    * copy configs into place
    
    * genric inspec tests
    
    * fix tests
    
    * server specific inspec tests
    
    * do not cancel in flight
    
    * fix generic tests

---
+++ servers/kamyshowdown/test/inspec.rb
@@ -0,0 +1,15 @@
+describe file('/opt/dayz-server/serverDZ.cfg') do
+    its('sha256sum') { should eq 'c399fb6cdccad4147bed53b84d5e042d1e5336c6bcc5364ee5da1bc5de7db128' }
+end
+
+describe file('/opt/dayz-server/mpmissions/dayzOffline.chernarusplus/init.c') do
+    its('sha256sum') { should eq '35159da2f2b4fbd213534398a55e239aefda2dd83989187b54599ab5fc57e08b' }
+end
+
+describe file('/opt/dayz-server/mpmissions/dayzOffline.chernarusplus/cfgplayerspawnpoints.xml') do
+    its('sha256sum') { should eq '82f6edeec5ac79d52baec4e287dfe8414e510a54f2c78e5f7ee1d41b273e3c13' }
+end
+
+describe file('/opt/dayz-server/mpmissions/dayzOffline.chernarusplus/db/globals.xml') do
+    its('sha256sum') { should eq 'a2fbeed33718d975c328af5815419c815ad1cb34fb813302a0668f49ba6e2c82' }
+end
---
+++ test/inspec/generic.rb
@@ -0,0 +1,39 @@
+# Generic dayz server tests
+
+[
+    '/opt/dayz-server/serverDZ.cfg',
+    '/opt/dayz-server/mpmissions/dayzOffline.chernarusplus/init.c',
+    '/opt/dayz-server/mpmissions/dayzOffline.chernarusplus/cfgplayerspawnpoints.xml',
+    '/opt/dayz-server/mpmissions/dayzOffline.chernarusplus/db/globals.xml', 
+].each do |bin|
+    describe file(bin) do
+        its('owner') { should eq 'dayz' }
+        its('group') { should eq 'dayz' }
+        its('type') { should cmp 'file' }
+    end
+end
+
+describe user('dayz') do
+    it { should exist }
+    its('group') { should eq 'dayz' }
+    its('lastlogin') { should eq nil }
+end
+
+describe group('dayz') do
+    it { should exist }
+end
+
+describe systemd_service('dayz-server') do
+    it { should be_installed }
+end
+
+describe port(2301) do
+    it { should be_listening }
+    its('protocols') { should include 'udp' }
+    its('addresses') { should_not include '127.0.0.1' }
+    its('addresses') { should include '0.0.0.0' }
+end
+
+describe processes('DayZServer') do
+    its('users') { should eq ['dayz'] }
+end
