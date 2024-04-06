4561642d4614a59e551558f44fc910f23b25012e
Joe Sirianni <joe.sirianni@observiq.com>
2024-04-06 18:26:19 -0400

    refactor weapon var scope

--- servers/kamyshowdown/mpmissions/dayzOffline.chernarusplus/init.c
+++ servers/kamyshowdown/mpmissions/dayzOffline.chernarusplus/init.c
@@ -121,15 +121,17 @@ class CustomMission: MissionServer
 			player.GetInventory().CreateInInventory(nades.GetRandomElement());
 		}
 
+		EntityAI weapon;
+
 		switch(gun.GetRandomElement()) {
 		  case "Flaregun":
 			player.GetInventory().CreateInInventory("Flaregun");
 			player.GetInventory().CreateInInventory("Ammo_Flare");
 			break;
 		  case "MakarovIJ70":
-		  	EntityAI weaponMakarovIJ70 = player.GetHumanInventory().CreateInHands("MakarovIJ70");
+		  	weapon = player.GetHumanInventory().CreateInHands("MakarovIJ70");
 			// player.SetQuickBarEntityShortcut(weapon, 0);
-			weaponMakarovIJ70.GetInventory().CreateAttachment("Mag_IJ70_8Rnd");
+			weapon.GetInventory().CreateAttachment("Mag_IJ70_8Rnd");
 			player.GetInventory().CreateInInventory("Mag_IJ70_8Rnd");
 			if (decide(70) == true) {
 				player.GetInventory().CreateInInventory("PistolSuppressor");
@@ -139,9 +141,9 @@ class CustomMission: MissionServer
 			}
 			break;
 		  case "FNX45":
-			EntityAI weaponFNX45 = player.GetHumanInventory().CreateInHands("FNX45");
+			weapon = player.GetHumanInventory().CreateInHands("FNX45");
 			// player.SetQuickBarEntityShortcut(weapon, 0);
-			weaponFNX45.GetInventory().CreateAttachment("Mag_FNX45_15Rnd");
+			weapon.GetInventory().CreateAttachment("Mag_FNX45_15Rnd");
 			player.GetInventory().CreateInInventory("Mag_FNX45_15Rnd");
 			if (decide(70) == true) {
 				player.GetInventory().CreateInInventory("PistolOptic");
@@ -154,9 +156,9 @@ class CustomMission: MissionServer
 			}
 			break;
 		  case "Glock19":
-			EntityAI weaponGlock19 = player.GetHumanInventory().CreateInHands("Glock19");
+			weapon = player.GetHumanInventory().CreateInHands("Glock19");
 			// player.SetQuickBarEntityShortcut(weapon, 0);
-			weaponGlock19.GetInventory().CreateAttachment("Mag_Glock_15Rnd");
+			weapon.GetInventory().CreateAttachment("Mag_Glock_15Rnd");
 			player.GetInventory().CreateInInventory("Mag_Glock_15Rnd");
 			if (decide(70) == true) {
 				player.GetInventory().CreateInInventory("PistolOptic");
@@ -169,18 +171,18 @@ class CustomMission: MissionServer
 			}
 			break;
 		  case "MKII":
-			EntityAI weaponMKII = player.GetHumanInventory().CreateInHands("MKII");
+			weapon = player.GetHumanInventory().CreateInHands("MKII");
 			// player.SetQuickBarEntityShortcut(weapon, 0);
-			weaponMKII.GetInventory().CreateAttachment("Mag_MKII_10Rnd");
+			weapon.GetInventory().CreateAttachment("Mag_MKII_10Rnd");
 			player.GetInventory().CreateInInventory("Mag_MKII_10Rnd");
 			if (decide(10) == true) {
 				player.GetInventory().CreateInInventory("Mag_MKII_10Rnd");
 			}
 			break;
 		  case "Colt1911":
-			EntityAI weaponColt1911 = player.GetHumanInventory().CreateInHands("Colt1911");
+			weapon = player.GetHumanInventory().CreateInHands("Colt1911");
 			// player.SetQuickBarEntityShortcut(weapon, 0);
-			weaponColt1911.GetInventory().CreateAttachment("Mag_1911_7Rnd");
+			weapon.GetInventory().CreateAttachment("Mag_1911_7Rnd");
 			player.GetInventory().CreateInInventory("Mag_1911_7Rnd");
 			if (decide(70) == true) {
 				player.GetInventory().CreateInInventory("PistolSuppressor");
@@ -190,9 +192,9 @@ class CustomMission: MissionServer
 			}
 			break;
 		  case "Engraved1911":
-			EntityAI weaponEngraved1911 = player.GetHumanInventory().CreateInHands("Engraved1911");
+			weapon = player.GetHumanInventory().CreateInHands("Engraved1911");
 			// player.SetQuickBarEntityShortcut(weapon, 0);
-			weaponEngraved1911.GetInventory().CreateAttachment("Mag_1911_7Rnd");
+			weapon.GetInventory().CreateAttachment("Mag_1911_7Rnd");
 			player.GetInventory().CreateInInventory("Mag_1911_7Rnd");
 			if (decide(70) == true) {
 				player.GetInventory().CreateInInventory("PistolSuppressor");
@@ -202,9 +204,9 @@ class CustomMission: MissionServer
 			}
 			break;
 		  case "Izh18":
-			EntityAI weaponIzh18 = player.GetHumanInventory().CreateInHands("Izh18");
+			weapon = player.GetHumanInventory().CreateInHands("Izh18");
 			// player.SetQuickBarEntityShortcut(weapon, 0);
-			weaponIzh18.GetInventory().CreateAttachment("Ammo_762x39");
+			weapon.GetInventory().CreateAttachment("Ammo_762x39");
 			player.GetInventory().CreateInInventory("Ammo_762x39");
 			break;
 		  case "Mosin9130":
@@ -217,24 +219,24 @@ class CustomMission: MissionServer
 			}
 			break;
 		  case "CZ527":
-			EntityAI weaponCZ527 = player.GetHumanInventory().CreateInHands("CZ527");
+			weapon = player.GetHumanInventory().CreateInHands("CZ527");
 			// player.SetQuickBarEntityShortcut(weapon, 0);
-			weaponCZ527.GetInventory().CreateAttachment("Ammo_762x39");
+			weapon.GetInventory().CreateAttachment("Ammo_762x39");
 			player.GetInventory().CreateInInventory("Ammo_762x39");
 			break;
 		  case "Winchester70":
-			EntityAI weaponWinchester70 = player.GetHumanInventory().CreateInHands("Winchester70");
+			weapon = player.GetHumanInventory().CreateInHands("Winchester70");
 			// player.SetQuickBarEntityShortcut(weapon, 0);
-			weaponWinchester70.GetInventory().CreateAttachment("Mag_IJ70_8Rnd");
+			weapon.GetInventory().CreateAttachment("Mag_IJ70_8Rnd");
 			player.GetInventory().CreateInInventory("Ammo_308Win");
 			if (decide(70) == true) {
 				player.GetInventory().CreateInInventory("HuntingOptic");
 			}
 			break;
 		  case "SKS":
-			EntityAI weaponSKS = player.GetHumanInventory().CreateInHands("SKS");
+			weapon = player.GetHumanInventory().CreateInHands("SKS");
 			// player.SetQuickBarEntityShortcut(weapon, 0);
-			weaponSKS.GetInventory().CreateAttachment("Ammo_762x39");
+			weapon.GetInventory().CreateAttachment("Ammo_762x39");
 			player.GetInventory().CreateInInventory("Ammo_762x39");
 			if (decide(70) == true) {
 				player.GetInventory().CreateInInventory("PUScopeOptic");
@@ -244,20 +246,20 @@ class CustomMission: MissionServer
 			}
 			break;
 		  case "Mp133Shotgun":
-			EntityAI weaponMp133Shotgun = player.GetHumanInventory().CreateInHands("Mp133Shotgun");
+			weapon = player.GetHumanInventory().CreateInHands("Mp133Shotgun");
 			// player.SetQuickBarEntityShortcut(weapon, 0);
 			player.GetInventory().CreateInInventory(shotgunAmmo.GetRandomElement());
 			break;
 		  case "Izh43Shotgun":
-			EntityAI weaponIzh43Shotgun = player.GetHumanInventory().CreateInHands("Izh43Shotgun");
+			weapon = player.GetHumanInventory().CreateInHands("Izh43Shotgun");
 			// player.SetQuickBarEntityShortcut(weapon, 0);
 			player.GetInventory().CreateInInventory(shotgunAmmo.GetRandomElement());
 			break;
 		  case "Saiga":
-			EntityAI weaponSaiga = player.GetHumanInventory().CreateInHands("Saiga");
+			weapon = player.GetHumanInventory().CreateInHands("Saiga");
 			// player.SetQuickBarEntityShortcut(weapon, 0);
 			TStringArray saigaMag = {"Mag_Saiga_5Rnd","Mag_Saiga_8Rnd","Mag_Saiga_Drum20Rnd"}
-			weaponSaiga.GetInventory().CreateAttachment(saigaMag.GetRandomElement());
+			weapon.GetInventory().CreateAttachment(saigaMag.GetRandomElement());
 			player.GetInventory().CreateInInventory(saigaMag.GetRandomElement());
 			if (decide(70) == true) {
 				player.GetInventory().CreateInInventory(saigaMag.GetRandomElement());
@@ -267,9 +269,9 @@ class CustomMission: MissionServer
 			}
 			break;
 		  case "CZ61":
-			EntityAI weaponCZ61 = player.GetHumanInventory().CreateInHands("CZ61");
+			weapon = player.GetHumanInventory().CreateInHands("CZ61");
 			// player.SetQuickBarEntityShortcut(weapon, 0);
-			weaponCZ61.GetInventory().CreateAttachment("Mag_CZ61_20Rnd");
+			weapon.GetInventory().CreateAttachment("Mag_CZ61_20Rnd");
 			player.GetInventory().CreateInInventory("Mag_CZ61_20Rnd");
 			if (decide(70) == true) {
 				player.GetInventory().CreateInInventory("PistolSuppressor");
@@ -279,9 +281,9 @@ class CustomMission: MissionServer
 			}
 			break;
 		  case "UMP45":
-			EntityAI weaponUMP45 = player.GetHumanInventory().CreateInHands("UMP45");
+			weapon = player.GetHumanInventory().CreateInHands("UMP45");
 			// player.SetQuickBarEntityShortcut(weapon, 0);
-			weaponUMP45.GetInventory().CreateAttachment("Mag_UMP_25Rnd");
+			weapon.GetInventory().CreateAttachment("Mag_UMP_25Rnd");
 			player.GetInventory().CreateInInventory("Mag_UMP_25Rnd");
 			if (decide(70) == true) {
 				player.GetInventory().CreateInInventory("PistolSuppressor");
@@ -294,9 +296,9 @@ class CustomMission: MissionServer
 			}
 			break;
 		  case "MP5K":
-			EntityAI weaponMP5K = player.GetHumanInventory().CreateInHands("MP5K");
+			weapon = player.GetHumanInventory().CreateInHands("MP5K");
 			// player.SetQuickBarEntityShortcut(weapon, 0);
-			weaponMP5K.GetInventory().CreateAttachment("Mag_MP5_30Rnd");
+			weapon.GetInventory().CreateAttachment("Mag_MP5_30Rnd");
 			player.GetInventory().CreateInInventory("Mag_MP5_30Rnd");
 			if (decide(70) == true) {
 				player.GetInventory().CreateInInventory("PistolSuppressor");
@@ -309,9 +311,9 @@ class CustomMission: MissionServer
 			}
 			break;
 		  case "AKS74U":
-			EntityAI weaponAKS74U = player.GetHumanInventory().CreateInHands("AKS74U");
+			weapon = player.GetHumanInventory().CreateInHands("AKS74U");
 			// player.SetQuickBarEntityShortcut(weapon, 0);
-			weaponAKS74U.GetInventory().CreateAttachment("Mag_AK74_30Rnd");
+			weapon.GetInventory().CreateAttachment("Mag_AK74_30Rnd");
 			player.GetInventory().CreateInInventory("Mag_AK74_30Rnd");
 			if (decide(70) == true) {
 				player.GetInventory().CreateInInventory("AK_Suppressor");
@@ -321,9 +323,9 @@ class CustomMission: MissionServer
 			}
 			break;
 		  case "FAL":
-			EntityAI weaponFAL = player.GetHumanInventory().CreateInHands("FAL");
+			weapon = player.GetHumanInventory().CreateInHands("FAL");
 			// player.SetQuickBarEntityShortcut(weapon, 0);
-			weaponFAL.GetInventory().CreateAttachment("Mag_FAL_20Rnd");
+			weapon.GetInventory().CreateAttachment("Mag_FAL_20Rnd");
 			player.GetInventory().CreateInInventory("Mag_FAL_20Rnd");
 			if (decide(70) == true) {
 				player.GetInventory().CreateInInventory(natoOptic.GetRandomElement());
@@ -333,10 +335,10 @@ class CustomMission: MissionServer
 			}
 			break;
 		  case "AKM":
-			EntityAI weaponAKM = player.GetHumanInventory().CreateInHands("AKM");
+			weapon = player.GetHumanInventory().CreateInHands("AKM");
 			// player.SetQuickBarEntityShortcut(weapon, 0);
 			TStringArray akmMag = {"Mag_AKM_30Rnd","Mag_AKM_Palm30Rnd","Mag_AKM_Drum75Rnd"}
-			weaponAKM.GetInventory().CreateAttachment(akmMag.GetRandomElement());
+			weapon.GetInventory().CreateAttachment(akmMag.GetRandomElement());
 			player.GetInventory().CreateInInventory(akmMag.GetRandomElement());
 			if (decide(70) == true) {
 				player.GetInventory().CreateInInventory(akmMag.GetRandomElement());
@@ -349,9 +351,9 @@ class CustomMission: MissionServer
 			}
 			break;
 		  case "AK101":
-			EntityAI weaponAK101 = player.GetHumanInventory().CreateInHands("AK101");
+			weapon = player.GetHumanInventory().CreateInHands("AK101");
 			// player.SetQuickBarEntityShortcut(weapon, 0);
-			weaponAK101.GetInventory().CreateAttachment("Mag_AK101_30Rnd");
+			weapon.GetInventory().CreateAttachment("Mag_AK101_30Rnd");
 			player.GetInventory().CreateInInventory("Mag_AK101_30Rnd");
 			if (decide(70) == true) {
 				player.GetInventory().CreateInInventory("PSO11Optic");
@@ -364,9 +366,9 @@ class CustomMission: MissionServer
 			}
 			break;
 		  case "AK74":
-			EntityAI weaponAK74 = player.GetHumanInventory().CreateInHands("AK74");
+			weapon = player.GetHumanInventory().CreateInHands("AK74");
 			// player.SetQuickBarEntityShortcut(weapon, 0);
-			weaponAK74.GetInventory().CreateAttachment("Mag_AK74_30Rnd");
+			weapon.GetInventory().CreateAttachment("Mag_AK74_30Rnd");
 			player.GetInventory().CreateInInventory("Mag_AK74_30Rnd");
 			if (decide(70) == true) {
 				player.GetInventory().CreateInInventory(sovietOptic.GetRandomElement());
@@ -379,10 +381,10 @@ class CustomMission: MissionServer
 			}
 			break;
 		  case "M4A1":
-			EntityAI weaponM4A1 = player.GetHumanInventory().CreateInHands("M4A1");
+			weapon = player.GetHumanInventory().CreateInHands("M4A1");
 			// player.SetQuickBarEntityShortcut(weapon, 0);
 			TStringArray m4Mag = {"Mag_STANAG_30Rnd","Mag_STANAGCoupled_30Rnd","Mag_CMAG_10Rnd","Mag_CMAG_20Rnd","Mag_CMAG_30Rnd","Mag_CMAG_40Rnd"}
-			weaponM4A1.GetInventory().CreateAttachment(m4Mag.GetRandomElement());
+			weapon.GetInventory().CreateAttachment(m4Mag.GetRandomElement());
 			player.GetInventory().CreateInInventory(m4Mag.GetRandomElement());
 			if (decide(70) == true) {
 				player.GetInventory().CreateInInventory(m4Mag.GetRandomElement());
@@ -395,9 +397,9 @@ class CustomMission: MissionServer
 			}
 			break;
 		  case "VSS":
-			EntityAI weaponVSS = player.GetHumanInventory().CreateInHands("VSS");
+			weapon = player.GetHumanInventory().CreateInHands("VSS");
 			// player.SetQuickBarEntityShortcut(weapon, 0);
-			weaponVSS.GetInventory().CreateAttachment("Mag_VSS_10Rnd");
+			weapon.GetInventory().CreateAttachment("Mag_VSS_10Rnd");
 			player.GetInventory().CreateInInventory("Mag_VSS_10Rnd");
 			if (decide(70) == true) {
 				player.GetInventory().CreateInInventory("PSO11Optic");
@@ -407,15 +409,15 @@ class CustomMission: MissionServer
 			}			
 			break;
 		  case "B95":
-			EntityAI weaponB95 = player.GetHumanInventory().CreateInHands("B95");
+			weapon = player.GetHumanInventory().CreateInHands("B95");
 			// player.SetQuickBarEntityShortcut(weapon, 0);
-			weaponB95.GetInventory().CreateAttachment("Ammo_308Win");
+			weapon.GetInventory().CreateAttachment("Ammo_308Win");
 			player.GetInventory().CreateInInventory("Ammo_308Win");
 			break;
 		  case "SVD":
-			EntityAI weaponSVD = player.GetHumanInventory().CreateInHands("SVD");
+			weapon = player.GetHumanInventory().CreateInHands("SVD");
 			// player.SetQuickBarEntityShortcut(weapon, 0);
-			weaponSVD.GetInventory().CreateAttachment("Mag_SVD_10Rnd");
+			weapon.GetInventory().CreateAttachment("Mag_SVD_10Rnd");
 			player.GetInventory().CreateInInventory("Mag_SVD_10Rnd");
 			if (decide(70) == true) {
 				player.GetInventory().CreateInInventory("PSO11Optic");
@@ -428,9 +430,9 @@ class CustomMission: MissionServer
 			}	
 			break;
 		  default:
-			EntityAI weaponDefault = player.GetHumanInventory().CreateInHands("AK74");
+			weapon = player.GetHumanInventory().CreateInHands("AK74");
 			// player.SetQuickBarEntityShortcut(weapon, 0);
-			weaponDefault.GetInventory().CreateAttachment("Mag_AK74_30Rnd");
+			weapon.GetInventory().CreateAttachment("Mag_AK74_30Rnd");
 			player.GetInventory().CreateInInventory("Mag_AK74_30Rnd");
 		}
 		
