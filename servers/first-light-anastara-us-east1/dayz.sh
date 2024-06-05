f67864a5ab60a3e9bc5eb10ba8933ef8537dcc2f
jsirianni <joe.sirianni@observiq.com>
2024-06-05 18:54:15 -0400

    required mods

--- servers/first-light-anastara-us-east1/dayz.sh
+++ servers/first-light-anastara-us-east1/dayz.sh
@@ -28,6 +28,11 @@ mod_online_tools="1564026768"
 mod_admin_tools="1582756848"
 mod_code_lock="1646187754"
 mod_anastara="2973953648"
+mod_anastara_pack="3076836722"
+mod_SimpleSpawner="2868802243"
+mod_SpawnerBubaku="2482312670"
+mod_Dabs="2545327648"
+mod_WindstridesClothingPack="1797720064"
 
 dayz() {
     sudo -u dayz 'bash' <<EOF
@@ -40,6 +45,11 @@ dayz() {
     +workshop_download_item "$DAYZ_WORKSHOP_ID" "$mod_admin_tools" \
     +workshop_download_item "$DAYZ_WORKSHOP_ID" "$mod_code_lock" \
     +workshop_download_item "$DAYZ_WORKSHOP_ID" "$mod_anastara" \
+    +workshop_download_item "$DAYZ_WORKSHOP_ID" "$mod_anastara_pack" \
+    +workshop_download_item "$DAYZ_WORKSHOP_ID" "$mod_SimpleSpawner" \
+    +workshop_download_item "$DAYZ_WORKSHOP_ID" "$mod_SpawnerBubaku" \
+    +workshop_download_item "$DAYZ_WORKSHOP_ID" "$mod_Dabs" \
+    +workshop_download_item "$DAYZ_WORKSHOP_ID" "$mod_WindstridesClothingPack" \
     +quit
 EOF
 }
@@ -52,12 +62,22 @@ symlinks() {
     sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_admin_tools" "/opt/dayz/$mod_admin_tools"
     sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_code_lock" "/opt/dayz/$mod_code_lock"
     sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_anastara" "/opt/dayz/$mod_anastara"
+    sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_anastara_pack" "/opt/dayz/$mod_anastara_pack"
+    sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_SimpleSpawner" "/opt/dayz/$mod_SimpleSpawner"
+    sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_SpawnerBubaku" "/opt/dayz/$mod_SpawnerBubaku"
+    sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_Dabs" "/opt/dayz/$mod_Dabs"
+    sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_WindstridesClothingPack" "/opt/dayz/$mod_WindstridesClothingPack"
 
     # CF and Online Tools share the same key
     eval sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_cf/keys/*" /opt/dayz/keys/
     eval sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_admin_tools/Keys/*" /opt/dayz/keys/
     eval sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_code_lock/Keys/*" /opt/dayz/keys/
     eval sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_anastara/Keys/*" /opt/dayz/keys/
+    eval sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_anastara_pack/Keys/*" /opt/dayz/keys/
+    eval sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_SimpleSpawner/Keys/*" /opt/dayz/keys/
+    eval sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_SpawnerBubaku/Keys/*" /opt/dayz/keys/
+    eval sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_Dabs/Keys/*" /opt/dayz/keys/
+    eval sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_WindstridesClothingPack/Keys/*" /opt/dayz/keys/
 
     sudo chown -R dayz:dayz /opt/dayz
 }
@@ -74,7 +94,7 @@ After=syslog.target network.target nss-lookup.target network-online.target
 ExecStart=/opt/dayz/DayZServer \
     -config=serverDZ.cfg \
     -port=2301 \
-    -mod="$mod_cf;$mod_online_tools;$mod_admin_tools;$mod_code_lock;$mod_anastara;" \
+    -mod="$mod_cf;$mod_online_tools;$mod_admin_tools;$mod_code_lock;$mod_anastara;$mod_anastara_pack;$mod_SimpleSpawner;$mod_SpawnerBubaku;$mod_Dabs;$mod_WindstridesClothingPack;" \
     -BEpath=battleye \
     -profiles=profiles \
     -dologs \
