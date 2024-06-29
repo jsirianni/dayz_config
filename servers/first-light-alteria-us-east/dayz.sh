7399ebbf7c14c1dc3bcb73348c2b8c7b37706139
Joe Sirianni <joe.sirianni@observiq.com>
2024-06-29 19:48:40 -0400

    true up mod list

--- servers/first-light-alteria-us-east/dayz.sh
+++ servers/first-light-alteria-us-east/dayz.sh
@@ -24,7 +24,7 @@ DAYZ_WORKSHOP_ID=221100
 shell_home="/opt/dayz"
 
 mod_cf="1559212036"
-mod_online_tools="1564026768"
+mod_vpp="1828439124"
 mod_code_lock="1646187754"
 
 dayz() {
@@ -34,7 +34,7 @@ dayz() {
     +login "$STEAM_USER" \
     +app_update "$DAYZ_APP_ID" \
     +workshop_download_item "$DAYZ_WORKSHOP_ID" "$mod_cf" \
-    +workshop_download_item "$DAYZ_WORKSHOP_ID" "$mod_online_tools" \
+    +workshop_download_item "$DAYZ_WORKSHOP_ID" "$mod_vpp" \
     +workshop_download_item "$DAYZ_WORKSHOP_ID" "$mod_code_lock" \
     +quit
 EOF
@@ -44,11 +44,12 @@ EOF
 # is capitalized.
 symlinks() {
     sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_cf" "/opt/dayz/$mod_cf"
-    sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_online_tools" "/opt/dayz/$mod_online_tools"
+    sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_vpp" "/opt/dayz/$mod_vpp"
     sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_code_lock" "/opt/dayz/$mod_code_lock"
 
     # CF and Online Tools share the same key
     eval sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_cf/keys/*" /opt/dayz/keys/
+    eval sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_vpp/keys/*" /opt/dayz/keys/
     eval sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_code_lock/Keys/*" /opt/dayz/keys/
 
     sudo chown -R dayz:dayz /opt/dayz
@@ -66,7 +67,7 @@ After=syslog.target network.target nss-lookup.target network-online.target
 ExecStart=/opt/dayz/DayZServer \
     -config=serverDZ.cfg \
     -port=2301 \
-    -mod="$mod_cf;$mod_online_tools;$mod_code_lock;" \
+    -mod="$mod_cf;$mod_vpp;$mod_code_lock;" \
     -BEpath=battleye \
     -profiles=profiles \
     -dologs \
