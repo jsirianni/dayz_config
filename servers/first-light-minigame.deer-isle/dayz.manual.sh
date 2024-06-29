43e1990facaad64490787e0158ca8dcbe0a58df5
Joseph Sirianni <joe.sirianni@bluemedora.com>
2024-06-29 19:13:05 -0400

    Minigame deer isle (#43)
    * copy from minigame and rename server name in config
    
    * update port
    
    * reset to base mods and update game port to 2601
    
    * Remove trade profile
    
    * rename map, remove init.c and spanws. retain globals to disable zombies and animals
    
    * bootstrap script for running when steam prompts for password and steam gaurd code
    
    * finish

---
+++ servers/first-light-minigame.deer-isle/dayz.manual.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Make sure user is "dayz"
+if [ "$(whoami)" != "dayz" ]; then
+    echo "Please run as dayz user"
+    exit 1
+fi
+
+# Pass steam username as argument
+if [ -z "$1" ]; then
+    echo "Usage: $0 <steam_username>"
+    exit 1
+fi
+
+STEAM_USER="$1"
+DAYZ_APP_ID=223350
+DAYZ_WORKSHOP_ID=221100
+
+shell_home="/opt/dayz"
+
+dayz() {
+/usr/games/steamcmd \
+    +force_install_dir "$shell_home" \
+    +login "$STEAM_USER" \
+    +app_update "$DAYZ_APP_ID" \
+    +quit
+}
+
+systemd() {
+    sudo tee /etc/systemd/system/dayz.service > /dev/null <<EOF
+[Unit]
+Description=DayZ Dedicated Server
+Wants=network-online.target
+After=syslog.target network.target nss-lookup.target network-online.target
+
+[Service]
+#ExecStartPre=/opt/update.sh
+ExecStart=/opt/dayz/DayZServer \
+    -config=serverDZ.cfg \
+    -port=2601 \
+    -BEpath=battleye \
+    -profiles=profiles \
+    -dologs \
+    -adminlog \
+    -netlog \
+    -freezecheck
+WorkingDirectory=/opt/dayz
+LimitNOFILE=100000
+ExecReload=/bin/kill -s HUP \$MAINPID
+ExecStop=/bin/kill -s INT \$MAINPID
+User=dayz
+Group=dayz
+Restart=always
+RestartSec=5s
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+}
+
+dayz
+systemd
---
+++ servers/first-light-minigame.deer-isle/dayz.sh
@@ -0,0 +1,108 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Pass steam username as argument
+if [ -z "$1" ]; then
+    echo "Usage: $0 <steam_username>"
+    exit 1
+fi
+
+if [ "$(id -u)" -ne 0 ]; then
+    # check if user has sudo privileges
+    sudo -v
+    if [ $? -ne 0 ]; then
+        echo "Please run as root or with sudo"
+        exit 1
+    fi
+fi
+
+STEAM_USER="$1"
+DAYZ_APP_ID=223350
+DAYZ_WORKSHOP_ID=221100
+
+shell_home="/opt/dayz"
+
+mod_cf="1559212036"
+mod_vpp="1828439124"
+mod_deerisle="1602372402"
+mod_party="1582671564"
+mod_buildanywhere="1854626456"
+mod_bbp="1710977250"
+
+dayz() {
+    sudo -u dayz 'bash' <<EOF
+/usr/games/steamcmd \
+    +force_install_dir "$shell_home" \
+    +login "$STEAM_USER" \
+    +app_update "$DAYZ_APP_ID" \
+    +workshop_download_item "$DAYZ_WORKSHOP_ID" "$mod_cf" \
+    +workshop_download_item "$DAYZ_WORKSHOP_ID" "$mod_vpp" \
+    +workshop_download_item "$DAYZ_WORKSHOP_ID" "$mod_deerisle" \
+    +workshop_download_item "$DAYZ_WORKSHOP_ID" "$mod_party" \
+    +workshop_download_item "$DAYZ_WORKSHOP_ID" "$mod_buildanywhere" \
+    +workshop_download_item "$DAYZ_WORKSHOP_ID" "$mod_bbp" \
+    +quit
+EOF
+}
+
+# symlinks links the mod and key directories. NOTE: Sometimes the key directory
+# is capitalized.
+symlinks() {
+    sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_cf" "/opt/dayz/$mod_cf"
+    sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_vpp" "/opt/dayz/$mod_vpp"
+    sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_deerisle" "/opt/dayz/$mod_deerisle"
+    sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_party" "/opt/dayz/$mod_party"
+    sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_buildanywhere" "/opt/dayz/$mod_buildanywhere"
+    sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_bbp" "/opt/dayz/$mod_bbp"
+
+    # CF and Online Tools share the same keyadmins.cfg
+
+    eval sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_cf/keys/*" /opt/dayz/keys/
+    eval sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_vpp/keys/*" /opt/dayz/keys/
+    eval sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_deerisle/Keys/*" /opt/dayz/keys/
+    eval sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_party/Keys/*" /opt/dayz/keys/
+    eval sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_buildanywhere/Keys/*" /opt/dayz/keys/
+    eval sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_bbp/keys/*" /opt/dayz/keys/
+
+    sudo chown -R dayz:dayz /opt/dayz
+}
+
+systemd() {
+    sudo tee /etc/systemd/system/dayz.service > /dev/null <<EOF
+[Unit]
+Description=DayZ Dedicated Server
+Wants=network-online.target
+After=syslog.target network.target nss-lookup.target network-online.target
+
+[Service]
+#ExecStartPre=/opt/update.sh
+ExecStart=/opt/dayz/DayZServer \
+    -config=serverDZ.cfg \
+    -port=2601 \
+    -mod="$mod_cf;$mod_vpp;$mod_deerisle;$mod_party;$mod_buildanywhere;$mod_bbp;" \
+    -BEpath=battleye \
+    -profiles=profiles \
+    -dologs \
+    -adminlog \
+    -netlog \
+    -freezecheck
+WorkingDirectory=/opt/dayz
+LimitNOFILE=100000
+ExecReload=/bin/kill -s HUP \$MAINPID
+ExecStop=/bin/kill -s INT \$MAINPID
+User=dayz
+Group=dayz
+Restart=always
+RestartSec=5s
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+}
+
+dayz
+symlinks
+systemd
