26acf75fc009d060a23146b5994be4cf51ca9bd6
jsirianni <joe.sirianni@observiq.com>
2024-12-02 16:18:23 -0500

    add kamy script

---
+++ servers/first-light-kamyshowdown/dayz.sh
@@ -0,0 +1,80 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Pass steam username as argument
+if [ -z "$1" ]; then
+    echo "Usage: $0 <steam_username>"
+    exit 1
+fi
+
+if [ "$(id -u)" -ne 0 ]; then
+    # check if user has sudo privileges
+    sudo -v
+    if [ $? -ne 0 ]; then
+        echo "Please run as root or with sudo"
+        exit 1
+    fi
+fi
+
+STEAM_USER="$1"
+DAYZ_APP_ID=223350
+DAYZ_WORKSHOP_ID=221100
+
+shell_home="/opt/dayz"
+
+dayz() {
+    sudo -u dayz 'bash' <<EOF
+/usr/games/steamcmd \
+    +force_install_dir "$shell_home" \
+    +login "$STEAM_USER" \
+    +app_update "$DAYZ_APP_ID" \
+    +quit
+EOF
+}
+
+# symlinks links the mod and key directories. NOTE: Sometimes the key directory
+# is capitalized.
+symlinks() {
+
+    sudo chown -R dayz:dayz /opt/dayz
+}
+
+systemd() {
+    sudo tee /etc/systemd/system/dayz.service > /dev/null <<EOF
+[Unit]
+Description=DayZ Dedicated Server
+Wants=network-online.target
+After=syslog.target network.target nss-lookup.target network-online.target
+
+[Service]
+#ExecStartPre=/opt/update.sh
+ExecStart=/opt/dayz/DayZServer \
+    -config=serverDZ.cfg \
+    -port=2701 \
+    -mod="" \
+    -BEpath=battleye \
+    -profiles=profiles \
+    -dologs \
+    -adminlog \
+    -netlog \
+    -freezecheck
+WorkingDirectory=/opt/dayz
+LimitNOFILE=100000
+ExecReload=/bin/kill -s HUP \$MAINPID
+ExecStop=/bin/kill -s INT \$MAINPID
+User=dayz
+Group=dayz
+Restart=always
+RestartSec=5s
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+}
+
+dayz
+symlinks
+systemd
