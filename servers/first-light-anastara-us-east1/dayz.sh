71ec41ddb86505b04e9fbc3a28f33d0238feff83
jsirianni <joe.sirianni@observiq.com>
2025-02-15 17:39:09 -0500

    add required mods

--- servers/first-light-anastara-us-east1/dayz.sh
+++ servers/first-light-anastara-us-east1/dayz.sh
@@ -26,6 +26,9 @@ shell_home="/opt/dayz"
 mod_cf="1559212036"
 mod_vpp="1828439124"
 mod_anastara="2973953648"
+mod_anastarta_pack="3076836722"
+dabs_framework="2545327648"
+windstrides="1797720064"
 
 dayz() {
     sudo -u dayz 'bash' <<EOF
@@ -36,6 +39,9 @@ dayz() {
     +workshop_download_item "$DAYZ_WORKSHOP_ID" "$mod_cf" \
     +workshop_download_item "$DAYZ_WORKSHOP_ID" "$mod_vpp" \
     +workshop_download_item "$DAYZ_WORKSHOP_ID" "$mod_anastara" \
+    +workshop_download_item "$DAYZ_WORKSHOP_ID" "$mod_anastarta_pack" \
+    +workshop_download_item "$DAYZ_WORKSHOP_ID" "$dabs_framework" \
+    +workshop_download_item "$DAYZ_WORKSHOP_ID" "$windstrides" \
     +quit
 EOF
 }
@@ -46,10 +52,16 @@ symlinks() {
     sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_cf" "/opt/dayz/$mod_cf"
     sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_vpp" "/opt/dayz/$mod_vpp"
     sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_anastara" "/opt/dayz/$mod_anastara"
+    sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_anastarta_pack" "/opt/dayz/$mod_anastarta_pack"
+    sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$dabs_framework" "/opt/dayz/$dabs_framework"
+    sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$windstrides" "/opt/dayz/$windstrides"
 
     eval sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_cf/keys/*" /opt/dayz/keys/
     eval sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_vpp/keys/*" /opt/dayz/keys/
     eval sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_anastara/keys/*" /opt/dayz/keys/
+    eval sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_anastarta_pack/keys/*" /opt/dayz/keys/
+    eval sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$dabs_framework/keys/*" /opt/dayz/keys/
+    eval sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$windstrides/keys/*" /opt/dayz/keys/
 
     sudo chown -R dayz:dayz /opt/dayz
 }
@@ -66,7 +78,7 @@ After=syslog.target network.target nss-lookup.target network-online.target
 ExecStart=/opt/dayz/DayZServer \
     -config=serverDZ.cfg \
     -port=3101 \
-    -mod="$mod_cf;$mod_vpp;$mod_anastara" \
+    -mod="$mod_cf;$mod_vpp;$mod_anastara;$mod_anastarta_pack;$dabs_framework;$windstrides" \
     -BEpath=battleye \
     -profiles=profiles \
     -dologs \
