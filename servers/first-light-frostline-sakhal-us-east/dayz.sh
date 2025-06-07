0f91bc498cc4b68f960a2a1b8dd1933475a7030d
jsirianni <joe.sirianni@observiq.com>
2025-06-07 12:19:05 -0400

    add codelock to frostline

--- servers/first-light-frostline-sakhal-us-east/dayz.sh
+++ servers/first-light-frostline-sakhal-us-east/dayz.sh
@@ -26,6 +26,7 @@ shell_home="/opt/dayz"
 mod_cf="1559212036"
 mod_vpp="1828439124"
 mod_buildanywhere="1854626456"
+mod_code_lock="1646187754"
 
 dayz() {
     sudo -u dayz 'bash' <<EOF
@@ -36,6 +37,7 @@ dayz() {
     +workshop_download_item "$DAYZ_WORKSHOP_ID" "$mod_cf" \
     +workshop_download_item "$DAYZ_WORKSHOP_ID" "$mod_vpp" \
     +workshop_download_item "$DAYZ_WORKSHOP_ID" "$mod_buildanywhere" \
+    +workshop_download_item "$DAYZ_WORKSHOP_ID" "$mod_code_lock" \
     +quit
 EOF
 }
@@ -46,10 +48,12 @@ symlinks() {
     sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_cf" "/opt/dayz/$mod_cf"
     sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_vpp" "/opt/dayz/$mod_vpp"
     sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_buildanywhere" "/opt/dayz/$mod_buildanywhere"
+    sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_code_lock" "/opt/dayz/$mod_code_lock"
 
     eval sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_cf/keys/*" /opt/dayz/keys/
     eval sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_vpp/keys/*" /opt/dayz/keys/
     eval sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_buildanywhere/Keys/*" /opt/dayz/keys/
+    eval sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_code_lock/Keys/*" /opt/dayz/keys/
 
     sudo chown -R dayz:dayz /opt/dayz
 }
@@ -66,7 +70,7 @@ After=syslog.target network.target nss-lookup.target network-online.target
 ExecStart=/opt/dayz/DayZServer \
     -config=serverDZ.cfg \
     -port=2301 \
-    -mod="$mod_cf;$mod_vpp;$mod_buildanywhere;" \
+    -mod="$mod_cf;$mod_vpp;$mod_buildanywhere;$mod_code_lock;" \
     -BEpath=battleye \
     -profiles=profiles \
     -dologs \
