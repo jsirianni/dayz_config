adc09428981d5be6561c61e8d73c112da872eca6
jsirianni <joe.sirianni@observiq.com>
2025-09-14 21:42:42 -0400

    more minigame mods

--- servers/first-light-minigame.chernerous/dayz.sh
+++ servers/first-light-minigame.chernerous/dayz.sh
@@ -29,6 +29,10 @@ mod_trader="1590841260"
 mod_map="1623711988"
 mod_party="1582671564"
 mode_boats="3354681846"
+thirdpp="2122332595"
+cars="3369325490"
+gunz="2443122116"
+mil_gear="1630943713"
 
 dayz() {
     sudo -u dayz 'bash' <<EOF
@@ -42,6 +46,10 @@ dayz() {
     +workshop_download_item "$DAYZ_WORKSHOP_ID" "$mod_map" \
     +workshop_download_item "$DAYZ_WORKSHOP_ID" "$mod_party" \
     +workshop_download_item "$DAYZ_WORKSHOP_ID" "$mode_boats" \
+    +workshop_download_item "$DAYZ_WORKSHOP_ID" "$thirdpp" \
+    +workshop_download_item "$DAYZ_WORKSHOP_ID" "$cars" \
+    +workshop_download_item "$DAYZ_WORKSHOP_ID" "$gunz" \
+    +workshop_download_item "$DAYZ_WORKSHOP_ID" "$mil_gear" \
     +quit
 EOF
 }
@@ -55,15 +63,40 @@ symlinks() {
     sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_map" "/opt/dayz/$mod_map"
     sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_party" "/opt/dayz/$mod_party"
     sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mode_boats" "/opt/dayz/$mode_boats"
+    sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$thirdpp" "/opt/dayz/$thirdpp"
+    sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$cars" "/opt/dayz/$cars"
+    sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$gunz" "/opt/dayz/$gunz"
+    sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mil_gear" "/opt/dayz/$mil_gear"
 
-    # CF and Online Tools share the same keyadmins.cfg
+    # Function to find and link keys directory for a mod
+    link_mod_keys() {
+        local mod_id="$1"
+        local mod_path="/opt/dayz/steamapps/workshop/content/221100/$mod_id"
+        
+        # Check for different key directory variations
+        for key_dir in "keys" "Keys" "key" "Key"; do
+            if [ -d "$mod_path/$key_dir" ]; then
+                echo "Found $key_dir directory for mod $mod_id"
+                eval sudo ln -sf "$mod_path/$key_dir/*" /opt/dayz/keys/
+                return 0
+            fi
+        done
+        
+        echo "Warning: No key directory found for mod $mod_id"
+        return 1
+    }
 
-    eval sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_cf/keys/*" /opt/dayz/keys/
-    eval sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_vpp/keys/*" /opt/dayz/keys/
-    eval sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_trader/keys/*" /opt/dayz/keys/
-    eval sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_map/Keys/*" /opt/dayz/keys/
-    eval sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mod_party/Keys/*" /opt/dayz/keys/
-    eval sudo ln -sf "/opt/dayz/steamapps/workshop/content/221100/$mode_boats/Keys/*" /opt/dayz/keys/
+    # Link keys for each mod
+    link_mod_keys "$mod_cf"
+    link_mod_keys "$mod_vpp"
+    link_mod_keys "$mod_trader"
+    link_mod_keys "$mod_map"
+    link_mod_keys "$mod_party"
+    link_mod_keys "$mode_boats"
+    link_mod_keys "$thirdpp"
+    link_mod_keys "$cars"
+    link_mod_keys "$gunz"
+    link_mod_keys "$mil_gear"
 
     sudo chown -R dayz:dayz /opt/dayz
 }
@@ -80,7 +113,7 @@ After=syslog.target network.target nss-lookup.target network-online.target
 ExecStart=/opt/dayz/DayZServer \
     -config=serverDZ.cfg \
     -port=2501 \
-    -mod="$mod_cf;$mod_vpp;$mod_trader;$mod_map;$mod_party;$mode_boats;" \
+    -mod="$mod_cf;$mod_vpp;$mod_trader;$mod_map;$mod_party;$mode_boats;$thirdpp;$cars;$gunz;$mil_gear;" \
     -BEpath=battleye \
     -profiles=profiles \
     -dologs \
